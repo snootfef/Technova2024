55d25e06a74418d3e8b4403e3d52930a7754731d
snootfef <delingucanada@gmail.com>
2024-09-28 19:49:13 -0400

    More features

--- technova2024/src/pages/nav.js
+++ technova2024/src/pages/nav.js
@@ -3,17 +3,16 @@ import { useState } from "react";
 import Link from "next/link"
 import Image from "next/image";
 import { FaGithub } from "react-icons/fa";
-import { CgProfile } from "react-icons/cg";
+import { BsPerson } from "react-icons/bs";
 import { usePathname } from "next/navigation";
 
 export default function Nav() {
 
-   const pages = ['/projects', '/designs', '/about']
    const pathname = usePathname()
 
    return (
       <nav
-         className='relative top-0 left-0 w-screen h-20 flex flex-row items-center text-white bg-white bg-opacity-50'
+         className='font-sans text-[0.92rem] px-20 pt-6 relative top-0 left-0 w-screen h-22 flex flex-row items-center text-white bg-opacity-50'
       >
          <div
             className="relative z-50 p-4 flex flex-grow justify-start duration-700 ease-in-out"
@@ -22,18 +21,17 @@ export default function Nav() {
                href='/'
                className=""
             >
-               <FaGithub />
-               {/* <Image
-                  src="/assets/logoSmall.png"
-                  className={"h-full w-full ease-in-out duration-200 hover:scale-105 " + (scroll || pages.includes(pathname) ? 'opacity-100' : 'opacity-0')}
+               <Image
+                  src="/assets/logo.png"
+                  className={"h-full w-full ease-in-out duration-200 hover:scale-[110%]"}
                   alt=""
-                  width="100"
+                  width="30"
                   height="1080"
-               /> */}
+               />
             </Link>
          </div>
          <a
-            className={'relative z-50 hover:scale-110 ease-in-out transition-all duration-200 ' + (pathname == '/' ? 'bg-slate-600' : '')}
+            className={'flex justify-center items-center relative z-50 ease-in-out transition-all duration-150 w-[8.5vw] h-[5.5vh] text-center rounded-lg ' + (pathname == '/' ? 'bg-white text-magenta mx-4' : 'text-white mx-1 hover:scale-110')}
             href="/"
          >
             <p
@@ -42,22 +40,28 @@ export default function Nav() {
                dashboard
             </p>
          </a>
-         <a className='relative z-50 pr-2 hover:scale-110 ease-in-out transition-all duration-200' href="/housemates">
+         <a
+            className={'flex justify-center items-center relative z-50 ease-in-out transition-all duration-150 w-[8.5vw] h-[5.5vh] text-center rounded-lg ' + (pathname == '/housemates' ? 'bg-white text-magenta mx-3' : 'text-white mx-1 hover:scale-110')}
+            href='/housemates'
+         >
             <p
                className='navText'
             >
                my roomis
             </p>
          </a>
-         <a className='relative z-50 pr-2 hover:scale-110 ease-in-out transition-all duration-200' href="/mytasks">
+         <a
+            className={'flex justify-center items-center relative z-50 ease-in-out transition-all duration-150 w-[8.5vw] h-[5.5vh] text-center rounded-lg ' + (pathname == '/mytasks' ? 'bg-white text-magenta mx-3' : 'text-white mx-1 hover:scale-110')}
+            href='/mytasks'
+         >
             <p
                className='navText'
             >
                my tasks
             </p>
          </a>
-         <a className='relative z-50 pr-2 hover:scale-110' href='mailto:d2gu@uwaterloo.ca' target="_blank" rel="noopener noreferrer">
-            <CgProfile
+         <a className='ml-3 relative z-50 hover:scale-110 ease-in-out transition-all duration-150' href='mailto:d2gu@uwaterloo.ca' target="_blank" rel="noopener noreferrer">
+            <BsPerson
                className="text-3xl"
             />
          </a>
