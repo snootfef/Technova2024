2ff8e2ce30ee0ae8c23827a0a4d208b266410bd3
steph <saliu1229@gmail.com>
2024-09-29 00:16:06 -0400

    changed housemates

--- technova2024/src/pages/housemates/index.js
+++ technova2024/src/pages/housemates/index.js
@@ -33,7 +33,6 @@ export async function getServerSideProps() {
       user0: "",
       user1: "",
       user2: "",
-      user3: "",
    };
 
    while (i < roommates.length) {
---
+++ technova2024/src/pages/secret.js
@@ -0,0 +1,4 @@
+const user = "saliu1229";
+const pass = "n97iBlZPJwSBB0LA";
+
+module.exports = { user, pass };
