0ff385ccee84c95ce5133214b8ae9b5e893ed09f
steph <saliu1229@gmail.com>
2024-09-28 23:13:04 -0400

    users are read from the database

--- technova2024/src/pages/housemates/HousemateCard.js
+++ technova2024/src/pages/housemates/HousemateCard.js
@@ -1,6 +1,7 @@
 import Image from "next/image"
 
-export default function HousemateCard() {
+export default function HousemateCard({name}) {
+   console.log(name);
 
    return (
       <main
@@ -15,16 +16,13 @@ export default function HousemateCard() {
             />
          </section>
          <div
-            className="mx-4 flex flex-col"
+            className="mx-4 flex flex-col w-full h-full"
          >
             <h2
-               className="name"
+               className="text-xl text-black"
             >
-               *Name
+               {name}
             </h2>
-            <p>
-               (437)555 0000
-            </p>
          </div>
       </main >
    )
--- technova2024/src/pages/housemates/index.js
+++ technova2024/src/pages/housemates/index.js
@@ -28,16 +28,35 @@ export async function getServerSideProps() {
    // iterate through list of roommates and query each user
    let i = 0;
    const users = database.collection("users");
+
+   const user_data = {
+      user0: "",
+      user1: "",
+      user2: "",
+      user3: "",
+   };
+
    while (i < roommates.length) {
       console.log(roommates[i]);
 
       //query for roommate
       query = {username: roommates[i]}
-      var user = await users.findOne(query);
+      var roommate = await users.findOne(query);
 
-      console.log(user);
+      // user_data["user"+i] = roommate;
+      if (roommate) {
+         // Convert _id to string
+         user_data["user" + i] = {
+            ...roommate,
+            _id: roommate._id.toString(), // Convert _id to string
+            // Convert Date fields to strings
+            date: roommate.date ? roommate.date.toISOString() : null, // Convert Date to ISO string if it exists
+         };
+      }
+
       i++;
    }
+   console.log(user_data)
 
    client.close();
 
@@ -45,13 +64,13 @@ export async function getServerSideProps() {
 
    return {
       props: {
-         data
+         user_data
       } // Pass the fetched data as props
    };
 
 }
 
-export default function Home() {
+export default function Home({user_data}) {
    return (
       <main className="relative flex flex-col w-screen h-screen no-scrollbar justify-center items-center">
          <Background />
@@ -59,9 +78,12 @@ export default function Home() {
          <div
             className='relative flex flex-wrap flex-row flex-1 justify-center items-center'
          >
-            <HousemateCard />
-            <HousemateCard />
-            <HousemateCard />
+            {Object.values(user_data).map((user, index) => (
+               <HousemateCard 
+                  key={user._id} // Use a unique identifier for the key
+                  name={user.username} // Adjust if the property name is different
+               />
+            ))}
          </div>
       </main>
    );
